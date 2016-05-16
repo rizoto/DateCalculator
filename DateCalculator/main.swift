df43e51c780ee35e1fee136ccfa9bafd50442158
Lubor Kolacny <lkolacny@me.com>
2016-05-16 22:07:15 +1000

    Initial Commit

---
+++ DateCalculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DateCalculator
+//
+//  Created by Lubor Kolacny on 16/05/2016.
+//  Copyright © 2016 Lubor Kolacny. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
