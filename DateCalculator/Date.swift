e4e769fc8cf93137713b8d643c70d12850d0fcb9
Lubor Kolacny <lkolacny@me.com>
2016-05-16 22:18:07 +1000

    added Test target

---
+++ DateCalculator/Date.swift
@@ -0,0 +1,63 @@
+//
+//  Date.swift
+//  DateCalculator
+//
+//  Created by Lubor Kolacny on 16/05/2016.
+//  Copyright © 2016 Lubor Kolacny. All rights reserved.
+//
+
+struct Date {
+
+    let daysInMonths = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    let dayOfYear = [0, 0,
+        31,
+        31 + 28,
+        31 + 28 + 31,
+        31 + 28 + 31 + 30,
+        31 + 28 + 31 + 30 + 31,
+        31 + 28 + 31 + 30 + 31 + 30,
+        31 + 28 + 31 + 30 + 31 + 30 + 31,
+        31 + 28 + 31 + 30 + 31 + 30 + 31 + 31,
+        31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30,
+        31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31,
+        31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30,
+        31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31]
+    
+    
+    let year, month, day : Int
+    
+    
+    init(year : Int, month : Int, day : Int) throws
+    {
+        
+        self.year = year
+        self.month = month
+        self.day = day
+        
+        // validation
+        if year == 0 || month == 0 || day == 0 {
+            throw DateValidationError.InvalidDate
+        } else if year < 1901 || year > 2999 {
+            throw DateValidationError.OutOfRange
+        } else if Date.leapYear(year) && month == 2 && day > 29 {
+            throw DateValidationError.InvalidDate
+        } else if month > 12 || daysInMonths[month] < day {
+            throw DateValidationError.InvalidDate
+        }
+    }
+    
+}
+
+extension Date {
+    
+    static func leapYear(year: Int) -> Bool {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+    }
+
+}
+
+enum DateValidationError : ErrorType {
+    case InvalidDate
+    case OutOfRange
+}
